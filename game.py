1a5d4fa404cb40032b124e29c83f3006d9fde512
LucaRogers <111995852+LucaRogers@users.noreply.github.com>
2023-05-13 02:33:46 +0000

    added documentation

--- game.py
+++ game.py
@@ -2,7 +2,8 @@ import pickle
 import sys
 import time
 import os
-
+#to play the game all you have to do is run the file. 
+#This is the loop I use for printing out text and making it look like it is being typed. 
 def type_out_text(text):
     for char in text:
         sys.stdout.write(char)
@@ -10,6 +11,7 @@ def type_out_text(text):
         time.sleep(0.05)
     print()
 
+#This is the intro of the game. The player gets asked the name and if they want to here the instructions. 
 playerName = input("What is your name?")
 play_intro = input(f"{playerName}, would you like to hear the intro and instructions?(input. 'yes'/'no')\n")
 while play_intro.lower() not in ["yes", "no"]:
@@ -27,6 +29,7 @@ To quit input 'quit'.
 Just so you know there are some hidden commands you have to discover ;)
 To see this message again input 'i'"""
     type_out_text(instructions)
+    #This is a useless input so whatever you put doesn't matter. I did this just to make sure the player has enough time to read the instructions before moving on. 
     input("Are you ready to begin this epic quest?\n")
     time.sleep(2)
     type_out_text("Honestly...you don't really have an option...Good luck!\n")
@@ -34,10 +37,12 @@ To see this message again input 'i'"""
 
 elif play_intro == "no":
     type_out_text("Good luck!")
-
+#this os.system('clear') is something I figured out how to use in order to clear everything in the terminal. It makes the game look more clear. 
 os.system('clear')
-
+#This is the room class where I define all the aspects of the room
+#The room has a name a description a series of possible exits and items.
 class Room():
+    #This method sets up the room and sets all of the NPC variables to their default values. 
     def __init__(self, name, description, exits, items = []):
         self.name = name
         self.description = description
@@ -47,6 +52,7 @@ class Room():
         self.dude_defeated = False
         self.cloaked = False
     
+    #the following update methods are used when the player defeats one of the NPC's. if they do the description of the rooms they are in changes. 
     def update(self):
         if self.sorcerer_defeated:
             self.description = "The sorcerer lies defeated on the ground. The room is now quiet."
@@ -64,7 +70,7 @@ class Room():
             self.description = "The cloaked figure lies dead on the floor."
         else:
             self.description = "You are closed off by iron bars. But wait... It looks like somebody broke through.\nThere is a cloaked figure in the corner."
-
+    #the display method displays all of the information of the current room you are in. 
     def display(self):
         print(self.name)
         type_out_text(self.description)
@@ -75,8 +81,9 @@ class Room():
             print("Items: ")
             for item in self.items:
                 print(item)
-
+#Game class
 class Game():
+    #The initializer takes in the things we need to know about the player. 
     def __init__(self):
         self.rooms = []
         self.current_room = None
@@ -90,25 +97,26 @@ To use an item input 'use' and then the item name.
 To save input 'save' to load input 'load'.
 To quit input 'quit'.
 To see this message again input 'i'"""
-    
+#There are very many methods which do their job, kind of hard to explain them all. 
     def add_room(self, room):
         self.rooms.append(room)
         
     def start(self):
         self.current_room = self.rooms[0]
         self.play()
-        
+    #This is the most important method that contains most of the game mechanics. 
     def play(self):
         while True:
             os.system('clear')
             self.current_room.display()
+            #Here we get the players input and automaticallly check if it corrisponds with the exits of current room you are in.
             command = input(f"\nWhat would you like to do?\n(Inventory:{self.inventory})\n").lower()
             if command in self.current_room.exits:
                  for room in self.rooms:
                     if room.name == self.current_room.exits[command]:
                         self.current_room = room
                         break
-            
+            #this is a command that the player is not told about. It is essential to talk to the librarian in order to win. 
             elif command == "talk" and self.current_room.name == "Library":
                 if self.smart:
                     type_out_text("You talk to a librarian who was hiding in the corner.")
@@ -121,43 +129,42 @@ There is a hidden trap door there.To go inside it simply input 'trap door' while
                 else:
                     type_out_text("I see you have discovered the clues...Now the treasure is yours. I am at peace.")
                     time.sleep(1)
-                
+            #another random unmentioned command.
             elif command == "sleep" and self.current_room.name == "Bedroom":
                 type_out_text("You take a short nap.")
-
+            #another random unmentioned command.
             elif command == "dance battle" and self.current_room.name == "Dungeon Cell":
                 type_out_text("You get schooled in a dance battle by a random guy in the corner. That must suck for you.")
            
-            elif command == "instructions":
-                print(instructions)
-            
             elif command == "quit":
                 break
             
             elif command == "i":
                 type_out_text(self.instructions)
-
+            #secret command that you can only do in the store room. Pretty hard to guess without talking to the librarian.
             elif command == "trap door" and self.current_room.name == "Storeroom":
+                #not sure why I added an achievment
                 type_out_text("achievement: Detective")
+                time.sleep(1)
                 self.current_room = room20
-
+            #yet another secret command
             elif command == "read" and self.current_room.name == "Graveyard":
                 type_out_text("The sorcerer is the man who murdered the late king Magnus.\nThe only way to defeat him is to use one of his potions.\nBeware of enemies for they are disguised as friends and the will betray you.\nSometimes leaving somebody alive will cost you your life.\nYou will meet a cloaked figure that you must not trust.\nYou will have to take advantage of him in order to survive.\nAfter you purchase a potion, you must end him.")
                 time.sleep(1)
-
+            #These save and load the various things in the game. 
             elif command == "save":
                 with open("save_game.pickle", "wb") as f:
-                    pickle.dump((self.current_room, self.inventory, self.smart, room3.items, room5.items, room10.items, room19.description), f)
+                    pickle.dump((self.current_room, self.inventory, self.smart, room3.items, room5.items, room10.items, room19.description, room13.description, room15.description), f)
                 type_out_text("Game saved.")
                     
             elif command == "load":
                 try:
                     with open("save_game.pickle", "rb") as f:
-                        self.current_room, self.inventory, self.smart, room3.items, room5.items, room10.items, room19.description = pickle.load(f)
+                        self.current_room, self.inventory, self.smart, room3.items, room5.items, room10.items, room19.description, room13.description, room15.description = pickle.load(f)
                     type_out_text("Game loaded.")
                 except FileNotFoundError:
                     type_out_text("No save file found.")
-        
+            #the way i decided to pick up items is to check if the players input starts with 'get'. If it does i seperate the get and the thing after it. Then if the thing after it exists, I check if I can use it based on what room I am in. In some cases it depends on other variables. 
             elif command.startswith("get "):
                 item_name = command[4:]
                 
@@ -168,10 +175,10 @@ There is a hidden trap door there.To go inside it simply input 'trap door' while
                     
                 else:
                     type_out_text(f"{item_name} is not in this room.")
-                    
+            #I did a similar thing i did with picking up items for the use items. I seperate the use and the word after and check if the word after exists. 
             elif command.startswith("use "):
                 item_name = command[4:]
-                
+                #starting here i have a bunch of different possibilities of items and how you can use them. A lot of them make you die. 
                 if item_name == "key" and "key" in self.inventory and self.current_room.name == "Treasure Room":
                     if self.smart:
                         type_out_text("You open the treasure chest but nothing is there. You must have missed something.\nHint: Talk to the librarian.")
@@ -194,7 +201,7 @@ There is a hidden trap door there.To go inside it simply input 'trap door' while
                     type_out_text("You pay a man and he gives you a potion in return")
                     self.inventory.remove("coin")
                     self.inventory.append("potion")
-                
+                #this is a stupid one
                 elif item_name == 'deodorant' and self.current_room.name == 'Lavatories':
                     type_out_text("Its still stinky...")
                 
@@ -208,6 +215,7 @@ There is a hidden trap door there.To go inside it simply input 'trap door' while
                     if "potion" in self.inventory:
                         type_out_text("You stab the cloaked figure. That was sad.")
                         self.inventory.remove("sword")
+                        #here is where I update the desciption of the room if the person inside of it was defeated or killed. I set the variable to true then call the update function. This is the same with room19 and room13. 
                         room15.cloaked = True
                         room15.update2()
         
@@ -251,7 +259,7 @@ There is a hidden trap door there.To go inside it simply input 'trap door' while
                     type_out_text("You can't use that here.")
             else:
                 type_out_text("Invalid command.")
-                
+#here is where i define all of the different rooms. A lot of them are useless.(meaning you can win the game without them.)I made all the rooms at the beginning stages of the game and it would be really tedious to try to remove a lot of them so I'm keeping them here. 
 room1 = Room("Entrance", "You are standing in front of a castle entrance.", {"north": "Foyer"})
 room2 = Room("Foyer", "You are standing in a grand foyer.", {"north": "Library", "east": "Dining Room", "south": "Entrance", "west": "Oratory"})
 room3 = Room("Library", "You are standing in a large library. There is a librarian in the corner.", {"south": "Foyer"}, ["book"])
@@ -273,7 +281,7 @@ room18 = Room("Sorcerers Room", "There is a powerful Sorcerer ready to kill you.
 room19 = Room("Treasure Room", "You have found the treasure room!", {})
 room20 = Room("Trap Door", "There is a trap door on the foor you didn't see before", {"north": "Graveyard", "south": "Storeroom"})
 room21 = Room("Graveyard", "There is some writing on a tombstone that you want to read.", {"south":"Trap Door"})
-
+#here i simply add all of the rooms to the game so you can access the items inside and go in them. 
 game = Game()
 game.add_room(room1)
 game.add_room(room2)
@@ -298,3 +306,4 @@ game.add_room(room20)
 game.add_room(room21)
 
 game.start()
+#the code is short but because of how complex it is it takes a long time. 
